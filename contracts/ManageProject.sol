513bfafadaa117e33fa5f0593aad9838153885ca
Bernard Peh <bpeh777@gmail.com>
2017-08-24 17:42:48 +1000

    update crowdsourcing tut

--- contracts/ManageProject.sol
+++ contracts/ManageProject.sol
@@ -5,12 +5,21 @@ import "./Project.sol";
 contract ManageProject {
 
 	mapping(uint => Project) public projects;
-	uint project_counter;
+	uint public project_counter;
+	// creator of this project
+	address admin;
 
 	event logCreatedProject(address project_address, address owner, bytes32 project_name, bytes32 project_desc, uint deadline);
-	event logUpdatedProject();
-	event logDeletedProject();
+	event logUpdatedProject(uint _id, address _owner, bytes32 _name, bytes32 _description);
+	event logDeletedProject(uint _id);
+	event logFundProject(uint _id, address _funder, uint _value);
 
+	// constructor
+	function ManageProject() {
+		admin = msg.sender;
+	}
+
+	// allows anyone to create new project
 	function createProject(bytes32 _name, bytes32 _desc, uint _deadline) {
 		address contract_address = new Project(msg.sender, _name, _desc, _deadline);
 		project_counter++;
@@ -18,6 +27,7 @@ contract ManageProject {
 		logCreatedProject(contract_address, msg.sender, _name, _desc, _deadline);
 	}
 
+	// converts bytes32 to string
 	function bytes32ToString(bytes32 x) constant returns (string) {
 		bytes memory bytesString = new bytes(32);
 		uint charCount = 0;
@@ -35,6 +45,7 @@ contract ManageProject {
 		return string(bytesStringTrimmed);
 	}
 
+	// get project struct anytime
 	function getProjectStruct(uint _id) returns (address, string, string, uint) {
 		address _owner;
 		bytes32 _name;
@@ -46,34 +57,72 @@ contract ManageProject {
 		return (_owner, _sname, _sdesc, _deadline);
 	}
 
-	function updateProjectStruct(uint id, address _owner, bytes32 _name, bytes32 _description) returns (bool) {
-		if (projects[_id].updateStruct(_owner, _name, _description)) {
-			return true;
-		}
-		else {
-			return false;
-		}
+	// project owner can update their own projects
+	function updateProjectStruct(uint _id, bytes32 _name, bytes32 _description) {
+		projects[_id].updateStruct(msg.sender, _name, _description);
+		logUpdatedProject(_id, msg.sender, _name, _description);
 	}
 
 	// only admin can delete project
 	function deleteProjectStruct(uint _id) returns (bool) {
-		// question: what is the difference between msg.sender and tx.origin?
-		require(msg.sender == tx.origin);
+		require(msg.sender == admin);
 		delete projects[_id];
+		logDeletedProject(_id);
 		return true;
 	}
+
+	// returns total funded
+	function fund(uint _id) payable returns (uint) {
+		// note the syntax for sending payment from a contract
+		logFundProject(_id, msg.sender, msg.value);
+		// whats the problem with this?
+		return projects[_id].fund.value(msg.value - 500000)(msg.sender);
+	}
+
+	// get funder
+	function getFunder(uint _id, address _funder) returns (uint) {
+		return projects[_id].getFunder(_funder);
+	}
+
+	// returns project current balance
+	function getProjectBalance(uint _id) returns (uint) {
+		projects[_id].getBalance();
+	}
+
+	// current project should not store any money. so it should always be 0.
+	function getContractBalance() returns (uint){
+		return this.balance;
+	}
+
+	// fallback. sorry dont accept free money.
+	function () {
+	}
+
 }
 
-// create project - should see new project created
-// ManageProject.deployed().then(function(ins){ins.createProject('hellow1 world', 'this is just a plain description', 50).then(function(val) {console.log(val)})})
+// create 2 projects - should see new project created
+// ManageProject.deployed().then(function(ins){ins.createProject('first project', 'this is just a plain description 1', 10).then(function(val) {console.log(val)})})
+// ManageProject.deployed().then(function(ins){ins.createProject('second project', 'this is just a plain description 2', 20).then(function(val) {console.log(val)})})
 
 // get project_counter
 // ManageProject.deployed().then(function(ins){ins.project_counter.call().then(function(val) {console.log(val.toString())}) })
 
-// see all project mappings
-// ManageProject.deployed().then(function(ins){ins.projects.call().then(function(val){console.log(val)})})
+// see project 2 address
+// ManageProject.deployed().then(function(ins){ins.projects.call(2).then(function(val){console.log(val)})})
 
-// get project
-// ManageProject.deployed().then(function(ins){ins.getProject(1).then(function(val){console.log(val)})})
+// current acct 3 balance
+// web3.fromWei(web3.eth.getBalance(web3.eth.accounts[3]));
+
+// fund project 2 from account 3
+// ManageProject.deployed().then(function(ins){ins.fund.sendTransaction(2, {from: web3.eth.accounts[3], value: 8300000000000000000}).then(function(val){console.log(val)})})
+
+// get funder balance
+// ManageProject.deployed().then(function(ins){ins.getFunder.call(2, web3.eth.accounts[3]).then(function(val){console.log(val)})})
+
+// get project 2 balance. Should be 2200.
+// ManageProject.deployed().then(function(ins){ins.getProjectBalance.call(2).then(function(val){console.log(val)})})
+
+// get current Contract Balance. Should be 0.
+// ManageProject.deployed().then(function(ins){ins.getContractBalance.call().then(function(val){console.log(val)})})
 
 // iterator - https://github.com/szerintedmi/solidity-itMapsLib
--- contracts/Project.sol
+++ contracts/Project.sol
@@ -3,20 +3,18 @@ pragma solidity ^0.4.4;
 contract Project {
 
 	struct PStruct {
+
 		address owner;
 		// exercise: why can't we use string here?
 		bytes32 name;
 		bytes32 description;
 		uint deadline;
+		mapping(address => uint) funders;
 	}
 
 	PStruct ps;
 
-	modifier ownerOnly(address _owner) {
-		require(_owner == ps.owner);
-		_;
-	}
-
+	// constructor
 	function Project(address _owner, bytes32 _name, bytes32 _description, uint _deadline) {
 		ps.owner = _owner;
 		ps.name = _name;
@@ -24,13 +22,46 @@ contract Project {
 		ps.deadline = _deadline;
 	}
 
+	// get struc values
 	function getStruct() returns (address, bytes32, bytes32, uint) {
 		return (ps.owner, ps.name, ps.description, ps.deadline);
 	}
 
 	// allows project owner to update project name and desc only
-	function updateStruc(bytes32 _name, bytes32 _description) ownerOnly() returns (bool) {
+	function updateStruct(address _owner, bytes32 _name, bytes32 _description) {
+		require(_owner == ps.owner);
 		ps.name = _name;
 		ps.description = _description;
+	}
+
+	// kill contract and payout
+	function kill() internal {
+		selfdestruct(ps.owner);
+	}
+
+	// allow anyone to fund the project
+	function fund(address _sender) payable returns (uint) {
+		// user cannot fund 0 ether
+		require(msg.value > 0);
+		ps.funders[_sender] += msg.value;
+		return ps.funders[_sender];
+	}
+
+	// get funder's amount so far
+	function getFunder(address _funder) returns (uint) {
+		return 	ps.funders[_funder];
+	}
+
+	// refund funder
+	function refund(address _funder) payable {
+		// if there are funds, transfer
+		if (ps.funders[_funder] > 0) {
+			_funder.transfer(ps.funders[_funder]);
+		}
+	}
+
+	// get current contract balance
+	function getBalance() returns (uint) {
+		return this.balance;
 	}
 }
