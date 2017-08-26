452511f04676c59038b0247275470173535171c4
bernard peh <bpeh@sitecritic.net>
2017-08-26 15:53:17 +1000

    update tut 2

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-var Basics = artifacts.require("./Basics.sol");
 var EventSales = artifacts.require("./EventSales.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Basics);
   deployer.deploy(EventSales);
 };
--- migrations/4_deploy_project.js
+++
@@ -1,7 +0,0 @@
-var ManageProject = artifacts.require("./ManageProject.sol");
-
-module.exports = function(deployer) {
-  // deployer.deploy(Project);
-  // deployer.link(Project, ManageProject);
-  deployer.deploy(ManageProject);
-};
